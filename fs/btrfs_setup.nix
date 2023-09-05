37ae8e4055710b4cfa92923d876df496c70c6522
lexi <tori@disroot.org>
2023-09-05 21:44:32 +0200

    make the script a bit more idiot-proof
    (its me, im the idiot)

--- fs/btrfs_setup.nix
+++ fs/btrfs_setup.nix
@@ -21,10 +21,22 @@ in pkgs.writeShellApplication {
             MNT=/setup/${devName}/
             mkdir -p "$MNT"
             mount ${dev.path} "$MNT"
+            : "Checking for existing subvolumes..."
+            if compgen -G "$MNT/@*"; then
+                : "Some subvolumes already exist on ${devName} (${dev.path})."
+                : "Do you want to proceed? Existing subvolumes will be re-used."
+                : "This might cause trouble if they're not compatible with the new config."
+                read -p "[y/N] " -r
+                if [[ ! $REPLY =~ ^[Yy]$ ]]; then umount $MNT; exit 1; fi
+            fi
             : "Creating subvolumes..."
             ${join "" (util.mapKeys subvols (sub: ''
-                : "Creating subvolume ${sub.name} at ${sub.mount}"
-                btrfs subvolume create "$MNT/${sub.name}"
+                : "Creating subvolume ${sub.name} at ${sub.mount} if it doesn't exist"
+                if [ ! -d "$MNT/${sub.name}" ]; then
+                    btrfs subvolume create "$MNT/${sub.name}";
+                else
+                    : "Subvolume ${sub.name} already exists! Re-using it, if something fails here fix it manually."
+                fi
                 ${join "" (util.mapKeys ([sub.mount] ++ (sub.additional_mounts or [])) (mount: ''
                     : "Mounting ${sub.name} at ${mount}"
                     mkdir -p "$ROOT${mount}"
